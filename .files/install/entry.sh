163de645054e9362e974dd7ac1993bb32821705c
BosEriko <bos.eriko@gmail.com>
2024-07-02 23:38:07 +0800

    rename youtube-dl to youtube

--- .files/install/entry.sh
+++ .files/install/entry.sh
@@ -2,7 +2,7 @@
 
 ~/.files/install/modules/apt.sh
 ~/.files/install/modules/fzf.sh
-~/.files/install/modules/youtube-dl.sh
+~/.files/install/modules/youtube.sh
 ~/.files/install/modules/node.sh
 ~/.files/install/modules/docker.sh
 ~/.files/install/modules/ruby.sh
