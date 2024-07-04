127ddd9c39147c81b967eff630989d961dad08a4
BosEriko <bos.eriko@gmail.com>
2024-07-04 13:41:49 +0800

    remove ngrok

--- .files/install/modules/node.sh
+++ .files/install/modules/node.sh
@@ -8,7 +8,6 @@ curl -fsSL https://fnm.vercel.app/install | bash
 echo "Installing: fnm has been installed."
 
 # Install packages using NPM (https://nodejs.org/)
-yarn global add ngrok                       # Introspected tunnels to localhost (https://github.com/inconshreveable/ngrok)
 yarn global add webtorrent-cli              # WebTorrent is the first BitTorrent client that works in the browser (https://github.com/webtorrent/webtorrent-cli)
 echo "Installing: NPM apps have been installed."
 
