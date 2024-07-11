f8e3160fa3659ed8aadedcc0e3f3e8af72dabad2
BosEriko <bos.eriko@gmail.com>
2024-07-11 20:30:36 +0800

    add yes to prompt on fzf

--- .files/install/modules/fzf.sh
+++ .files/install/modules/fzf.sh
@@ -2,5 +2,5 @@
 
 # fzf (https://github.com/junegunn/fzf)
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+yes | ~/.fzf/install
 echo "Installing: fzf has been installed."
