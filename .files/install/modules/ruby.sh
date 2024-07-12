2ea3dfb9cf7c55e9fa6a13ae698263e752f51c8d
BosEriko <bos.eriko@gmail.com>
2024-07-12 11:58:29 +0800

    revert ruby version

--- .files/install/modules/ruby.sh
+++ .files/install/modules/ruby.sh
@@ -8,8 +8,8 @@ git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-b
 git -C "$(rbenv root)"/plugins/ruby-build pull
 
 # Initialize rbenv and set global ruby version (https://github.com/rbenv/rbenv)
-sudo rbenv install 3.0.2
-sudo rbenv global 3.0.2
+sudo rbenv install 2.7.1
+sudo rbenv global 2.7.1
 sudo rbenv rehash
 
 # Install packages using Gem (https://rubygems.org/)
