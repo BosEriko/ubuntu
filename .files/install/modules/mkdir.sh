4526be07039d8988c8e2e9880af9b9c80f705bed
BosEriko <bos.eriko@gmail.com>
2024-07-12 11:14:14 +0800

    remove default dir when making directories

--- .files/install/modules/mkdir.sh
+++ .files/install/modules/mkdir.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 # Make the directories for Personal/Work
-cd ~ && mkdir -p Documents/Codes/Work
-cd ~ && mkdir -p Documents/Codes/Personal
+mkdir -p ~/Documents/Codes/Work ~/Documents/Codes/Personal
 
 # Make the directory for your Movies
-cd ~ && mkdir -p $STORAGE/Movies
+mkdir -p $STORAGE/Movies
 
 # Make the directory for your Anime
-cd ~ && mkdir -p $STORAGE/Anime
 
 # Make the directory for YouTube videos
-cd ~ && mkdir -p $STORAGE/YouTube
+mkdir -p $STORAGE/YouTube
