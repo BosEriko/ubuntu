13e6a080d8c476fb8d50af14232cb7335cdd350d
BosEriko <bos.eriko@gmail.com>
2024-07-12 10:40:36 +0800

    add ruby dev

--- .files/install/modules/apt.sh
+++ .files/install/modules/apt.sh
@@ -2,12 +2,13 @@
 
 # Declare an array of packages to be installed
 packages=(
-  zsh                          # Zsh is a shell designed for interactive use (http://www.zsh.org/)
-  rbenv                        # Groom your app’s Ruby environment (https://github.com/rbenv/rbenv)
-  figlet                       # FIGlet is a program for making large letters out of ordinary text (http://www.figlet.org/)
-  postgresql                   # PostgreSQL, a free and open-source relational database management system (https://www.postgresql.org)
-  postgresql-contrib           # Additional utilities for PostgreSQL
-  zsh-antigen                  # The plugin manager for zsh
+  zsh                         # Zsh is a shell designed for interactive use (http://www.zsh.org/)
+  rbenv                       # Groom your app’s Ruby environment (https://github.com/rbenv/rbenv)
+  figlet                      # FIGlet is a program for making large letters out of ordinary text (http://www.figlet.org/)
+  postgresql                  # PostgreSQL, a free and open-source relational database management system (https://www.postgresql.org)
+  postgresql-contrib          # Additional utilities for PostgreSQL
+  zsh-antigen                 # The plugin manager for zsh
+  ruby-dev                    # Ruby development headers
 )
 
 # Install packages from the array
