395696fcd1cce513bac72e2f6004d3a03c384ec2
BosEriko <bos.eriko@gmail.com>
2024-07-12 15:08:44 +0800

    add sudo for mkdir

--- .files/install/modules/mkdir.sh
+++ .files/install/modules/mkdir.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Make the directories for Personal/Work
+echo "Make the directories for Personal/Work"
 sudo mkdir -p ~/Documents/Codes/Work ~/Documents/Codes/Personal
 
-# Make the directory for your Movies
+echo "Make the directory for your Movies"
 sudo mkdir -p $STORAGE/Movies
 
-# Make the directory for your Anime
+echo "Make the directory for your Anime"
 sudo mkdir -p $STORAGE/Anime
 
-# Make the directory for YouTube videos
+echo "Make the directory for YouTube videos"
 sudo mkdir -p $STORAGE/YouTube
