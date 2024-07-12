e9fa0d03dbae08db0c2fcb057cb7bbc97442b06e
BosEriko <bos.eriko@gmail.com>
2024-07-12 10:23:32 +0800

    update version of ruby to 3.0.2

--- .files/install/modules/ruby.sh
+++ .files/install/modules/ruby.sh
@@ -8,8 +8,8 @@ git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-b
 git -C "$(rbenv root)"/plugins/ruby-build pull
 
 # Initialize rbenv and set global ruby version (https://github.com/rbenv/rbenv)
-rbenv install 2.7.1
-rbenv global 2.7.1
+rbenv install 3.0.2
+rbenv global 3.0.2
 rbenv rehash
 
 # Install packages using Gem (https://rubygems.org/)
