c1142fa9993476cd768b56c5e9a397c4cba02865
BosEriko <bos@eriko.ph>
2025-09-08 16:57:13 +0800

    Feature: Remove Sudo on making of Directories

--- .files/install/modules/mkdir.sh
+++ .files/install/modules/mkdir.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "Make the directories for Personal/Work"
-sudo mkdir -p ~/Documents/Codes/Work ~/Documents/Codes/Personal
+mkdir -p ~/Documents/Codes/Work ~/Documents/Codes/Personal
 
 echo "Make the directory for your Movies"
-sudo mkdir -p $STORAGE/Movies
+mkdir -p $STORAGE/Movies
 
 echo "Make the directory for your Anime"
-sudo mkdir -p $STORAGE/Anime
+mkdir -p $STORAGE/Anime
 
 echo "Make the directory for YouTube videos"
-sudo mkdir -p $STORAGE/YouTube
+mkdir -p $STORAGE/YouTube
