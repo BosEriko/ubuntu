15d9dcf7f5b51edeb0c935ec11d13b0afacc41d1
BosEriko <bos@eriko.ph>
2025-09-10 16:35:34 +0800

    Feature: Create a TODO script (ZSH)

--- .files/install/modules/apt.sh
+++ .files/install/modules/apt.sh
@@ -2,10 +2,11 @@
 
 # Declare an array of packages to be installed
 packages=(
-  zsh                         # Zsh is a shell designed for interactive use, offering powerful features and customization options.
   figlet                      # FIGlet is a program for making large letters out of ordinary text, used for banners in terminal.
+  jq                          # jq is a lightweight and flexible command-line JSON processor, required for Todoist integration.
   postgresql                  # PostgreSQL, a free and open-source relational database management system for robust data storage.
   postgresql-contrib          # Additional utilities and functionality for PostgreSQL, enhancing its capabilities.
+  zsh                         # Zsh is a shell designed for interactive use, offering powerful features and customization options.
   zsh-antigen                 # A tool for managing Zsh configurations, including themes and plugins.
 )
 
--- .zshrc
+++ .zshrc
@@ -11,3 +11,4 @@ source ~/.files/zsh/git.z.sh
 source ~/.files/zsh/personal.z.sh
 source ~/.files/zsh/session.z.sh
 source ~/.files/zsh/hotfix.z.sh
+source ~/.files/zsh/todo.z.sh
